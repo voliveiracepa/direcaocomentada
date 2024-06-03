18833cb3218307886bd0573c8dd6eb99363f409b
voliveiracepa <voliveira@cepasafedrive.com>
2024-06-03 16:19:27 -0300

    change db

--- connector.py
+++ connector.py
@@ -2,6 +2,5 @@ from pymongo import MongoClient
 
 
 def get_database(an):
-
     client = MongoClient('mongodb+srv://voliveira:FgeWAmJhW29aYBiF@direcaocomentada.nlwaoo3.mongodb.net/?retryWrites=true&w=majority&appName=direcaocomentada')
     return client[an]
--- controller/routes.py
+++ controller/routes.py
@@ -5,6 +5,32 @@ db = get_database('direcaocomentada')
 
 collection = db['astrazeneca']
 
+collection2 = db['astrazenecaNovo']
+
+data = {
+  "createdAt": "12/05/1990",
+  "driver":{
+    "name":"Jhson Doe",
+    "email":"test@test.com",
+    "corporationId":"15524",
+    "birthDate":"12/05/1950",
+    "joinDate":"12/05/1990"
+  },
+  "event": {
+    "eventDate" : "12/05/1999",
+    "eventTime": "08:00:00",
+    "local":"Street123",
+    "UF": "SP",
+    "isWorking":"sim",
+    "isMyFault": "nao",
+    "ADLCall":"123"
+  },
+  "roadCondictions":{
+    "illumination":"escuro",
+    "roadType":"em bom estado",
+    "type":"seco"
+  }
+}
 class Routes:
     def __init__(self, app):
         self.app = app
@@ -17,7 +43,7 @@ class Routes:
     def save(self):
         try:
             value_from_front = request.json
-            collection.insert_one(value_from_front)
+            collection2.insert_one(value_from_front)
 
             return jsonify({"message": "Dados recebidos e salvos com sucesso!"}), 200
         except Exception as e:
